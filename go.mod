b436b36630c3be28cb86c41c832e1ee3e1da616f
Dave Rowntree <drowntree@dxc.com>
2020-08-26 08:42:51 +0100

    redis implementation

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/daveo24/golang-webapp-setup
+
+go 1.15
+
+require (
+	github.com/go-redis/redis/v8 v8.0.0-beta.8
+	github.com/gorilla/mux v1.8.0
+)
--- webApp.go
+++ webApp.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"context"
 	"html/template"
 	"net/http"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
 )
 
 var templates *template.Template
+var client *redis.Client
+var ctx = context.Background()
 
 func main() {
 	templates = template.Must(templates.ParseGlob("templates/*.html"))
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379", // default redis address
+	})
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexPage).Methods("GET")
 	http.Handle("/", r)
 	http.ListenAndServe(":8000", nil)
+
 }
 
 func indexPage(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index.html", nil)
+	comments, err := client.LRange(ctx, "comments", 0, 20).Result()
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", comments)
 }
